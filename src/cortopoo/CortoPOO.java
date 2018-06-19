2d9c89d6837bfab03992cad2f625480976aedfcf
LN710Q <LN710Q@DEILABO1-PC06>
2018-06-19 07:19:22 -0600

    Inicio de corto de POO

---
+++ src/cortopoo/CortoPOO.java
@@ -0,0 +1,17 @@
+
+package cortopoo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class CortoPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
